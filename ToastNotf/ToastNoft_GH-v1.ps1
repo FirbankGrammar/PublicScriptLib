dc00feb773777e3e6cfb7a90e504479cb6515663
MaximC <mybean181@gmail.com>
2022-11-29 12:56:58 +1100

    Update ToastNoft_GH-v1.ps1

--- ToastNotf/ToastNoft_GH-v1.ps1
+++ ToastNotf/ToastNoft_GH-v1.ps1
@@ -8,8 +8,6 @@ USED VIA TASK SCHEDULER
 
 #>
 
-#> Bypasses script policies & enables TLS1.2
-
 $progressbar = New-BTProgressBar -Title 'Please save your open files' -Status 'Automatically restarting in' -Value 0.3 -ValueDisplay '45 minutes'
 New-BurntToastNotification -Text 'Critical Updates Pending', 'Your device is required to restart' -UniqueIdentifier 'pb_45' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 5
 
