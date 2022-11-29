c248ea0455909fc9a7867bf9895d4f11a32ac282
MaximC <mybean181@gmail.com>
2022-11-29 13:41:45 +1100

    Update ToastNoft_GH-v1.ps1

--- ToastNotf/ToastNoft_GH-v1.ps1
+++ ToastNotf/ToastNoft_GH-v1.ps1
@@ -8,8 +8,23 @@ USED VIA TASK SCHEDULER
 
 #>
 
+$Text = 'Critical Updates Pending', 'Your device is required to restart'
+
 $progressbar = New-BTProgressBar -Title 'Please save your open files' -Status 'Automatically restarting in' -Value 0.3 -ValueDisplay '45 minutes'
-New-BurntToastNotification -Text 'Critical Updates Pending', 'Your device is required to restart' -UniqueIdentifier 'pb_45' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 5
+New-BurntToastNotification -Text $Text -UniqueIdentifier 'pb_45' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 5
 
+Start-Sleep -Seconds 1200
 
+$progressbar = New-BTProgressBar -Title 'Please save your open files' -Status 'Automatically restarting in' -Value 0.6 -ValueDisplay '25 minutes'
+New-BurntToastNotification -Text $Text -UniqueIdentifier 'pb_25' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 5
+
+Start-Sleep -Seconds 1200
+
+$progressbar = New-BTProgressBar -Title 'Please save your open files' -Status 'Automatically restarting in' -Value 0.9 -ValueDisplay '5 minutes'
+New-BurntToastNotification -Text $Text -UniqueIdentifier 'pb_5' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 5
+
+Start-Sleep -Seconds 300
+
+$progressbar = New-BTProgressBar -Title 'Please save your open files' -Status 'Automatically restarting in' -Value 0.98 -ValueDisplay '1 minute'
+New-BurntToastNotification -Text $Text -UniqueIdentifier 'pb_1' -ProgressBar $progressbar -AppLogo 'https://i.imgur.com/ZUVZM3a.png' | Start-Sleep -Seconds 55
 
